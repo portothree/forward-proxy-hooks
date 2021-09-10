1ef832caf95360ddcfd5d7cd8679e522a7c36a4a
Gustavo Porto <gustavoporto@ya.ru>
2021-09-10 11:46:15 +0100

    fix: gchat from env var

--- routes/cronitor.js
+++ routes/cronitor.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 
 const router = express.Router();
-const GCHAT_URL = procecess.env.GCHAT_URL;
+const GCHAT_URL = process.env.GCHAT_URL;
 
 /*
  * Cronitor Webhook POST body will be in JSON:
