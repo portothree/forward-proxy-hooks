7b663c41724e839eacca528e2cb8e90aa9b1c67b
Gustavo Porto <gustavoporto@ya.ru>
2021-09-10 11:40:32 +0100

    feat: cronitor to gchat

--- routes/cronitor.js
+++ routes/cronitor.js
@@ -1,8 +1,29 @@
 const express = require('express');
-const router = express.Router();
+const axios = require('axios');
 
-router.get('/', function (req, res, next) {
-	res.send('respond with a resource');
+const router = express.Router();
+const GCHAT_URL = procecess.env.GCHAT_URL;
+
+/*
+ * Cronitor Webhook POST body will be in JSON:
+ *{
+ * "id": "The unique tracking code of this monitor",
+ * "type": "Alert OR Recovery",
+ * "monitor": "The name of this monitor",
+ * "description": "Why the alert was sent",
+ * "rule": "Name of the rule that was triggered"
+ *}
+ */
+
+router.post('/gchat', (req, res, next) => {
+	const { id, type, monitor, description, rule } = req.body;
+
+	axios.post(GCHAT_URL, {
+		text: `${monitor} | ${description}`,
+	});
+
+	res.status(200);
+	return res.json({ message: 'Sent' });
 });
 
 module.exports = router;
